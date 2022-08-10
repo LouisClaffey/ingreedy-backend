3faaa039b2e147a7f20ac0169e9c1d2ee4070e11
Louis Claffey <85559012+LouisClaffey@users.noreply.github.com>
2022-08-10 23:37:57 +0100

    taking out security

--- src/main/java/com/ingreedy/hdsdevproject/BcryptEncoderGenerator.java
+++ src/main/java/com/ingreedy/hdsdevproject/BcryptEncoderGenerator.java
@@ -1,19 +1,19 @@
-package com.ingreedy.hdsdevproject;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class BcryptEncoderGenerator {
-
-    /** used to generate 10 encrypted passwords */
-
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-        for(int i=0; i<10; i++) {
-            String encodedPw = encoder.encode("test");
-            System.out.println(encodedPw);
-        }
-
-
-    }
-}
+//package com.ingreedy.hdsdevproject;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//public class BcryptEncoderGenerator {
+//
+//    /** used to generate 10 encrypted passwords */
+//
+//    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//        for(int i=0; i<10; i++) {
+//            String encodedPw = encoder.encode("test");
+//            System.out.println(encodedPw);
+//        }
+//
+//
+//    }
+//}
